70797f8197270fb994ca9601a1d9cfe5e67528c6
Nhat Nguyen <31426677+nguyen-nhat@users.noreply.github.com>
2018-09-11 01:26:50 -0700

    Update app.py (Add signup)

--- project/app.py
+++ project/app.py
@@ -1,23 +1,30 @@
 # This is where the python flask code occupies
 
 from flask import Flask, render_template, redirect, url_for, request, Blueprint, session
-from modules.reader import checkLogin
+from modules.reader import checkLogin, checkEmail, refreshDatabase, getDatabase, userCreation
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret_key"
 
 main = Blueprint("main", __name__)
 
-
-# Main page for login
+# Redirects to the login page
 @main.route("/")
-def home():
+def index():
+  return redirect(url_for('main.login'))
+
+# User login page
+@main.route("/login")
+def login():
+  print()
+  print("I WAS HERE 3")
+  print()
   return render_template("login.html")
 
-# User login logic
-@main.route("/", methods=["GET", "POST"])
-def login():
-  # Stores user information 
+# Handles the login page logic
+@main.route("/login", methods=["GET", "POST"])
+def login_redirect():
+  # Stores login user information 
   session["email"] = request.form["email"]
   session["password"] = request.form["password"]
 
@@ -27,8 +34,35 @@ def login():
   else:
     return render_template("login.html", loginFailure=True)
 
+
+# User signin page
+@main.route("/signup")
+def signup():
+  return render_template("signup.html")
+
+# If the user is sucessfully created
+@main.route("/success")
+def signup_success():
+  return render_template("login.html", userCreation=True)
+
+# Handles the sign up page logic
+@main.route("/signup", methods=["GET", "POST"])
+def signup_redirect():
+  # Stores signup user information
+  session["fname"] = request.form["fname"]
+  session["lname"] = request.form["lname"]
+  session["email"] = request.form["email"]
+  session["password"] = request.form["password"]
+
+  # If email is unique, create the user in the database
+  if not checkEmail(session["email"]):
+    return redirect(url_for('main.signup_success'))
+  else:
+    return render_template("signup.html", loginFailure=True)
+
+
 # TODO: Finish the dashboard page
-# Dashboard page
+# User dashboard page
 @main.route("/app")
 def dashboard():
   return render_template("app.html", email=session["email"], password=session["password"])
