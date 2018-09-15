4093025c2eed1483a7961b2d799f42b6661a67c4
Nhat Nguyen <31426677+nguyen-nhat@users.noreply.github.com>
2018-09-15 04:33:40 -0700

    Update reader.py

--- project/modules/reader.py
+++ project/modules/reader.py
@@ -1,85 +1,73 @@
 # Python module checks if the pass user information matches with the database
 
+# TODO: Optimize the reading and writing database
+
 # Import pandas
 import pandas as pd
+import timeit
 
 # Convert DataFrame column to list
 def convert(df_column):
-  list = []
-  for element in df_column:
-    list.append(element)
-  return list
+    list = []
+    for element in df_column:
+        list.append(element)
+    return list
 
 # Checks if the email exist in the database
 def checkEmail(email):
-  if email in list_email:
-    return(True)
-  else:
-    return(False)
+    if email in list_email:
+      return(True)
+    else:
+       return(False)
 
 # Checks if the email match the password
 def checkPassword(email, password):
-  if password == dict_email_password[email]:
-    return(True)
-  else:
-    return(False)
+    if password == dict_email_password[email]:
+        return(True)
+    else:
+        return(False)
 
 # Checks form information with the database
 def checkLogin(email, password):
-  if checkEmail(email):
-    return(checkPassword(email, password))
-  else:
-    return(False)
+    if checkEmail(email):
+        return(checkPassword(email, password))
+    else:
+        return(False)
 
 # Create users: If email is unique, add user to the database
 def userCreation(fname, lname, email, password):
-  if checkEmail(email):
-    return False
-  else:
-    list_fname.append(fname)
-    list_lname.append(lname)
-    list_email.append(email)
-    list_password.append(password)
-    dict_email_password[email] = password
+    if checkEmail(email):
+        return False
+    else:
+        list_fname.append(fname)
+        list_lname.append(lname)
+        list_email.append(email)
+        list_password.append(password)
+        dict_email_password[email] = password
 
-    # Create a Pandas dataframe from the data.
-    df = pd.DataFrame({'First Name': list_fname, 'Last Name': list_lname,
-                       'Email': list_email, 'Password': list_password})
+        # Create a Pandas dataframe from the data.
+        df = pd.DataFrame({'First Name': list_fname, 'Last Name': list_lname, 'Email': list_email, 'Password': list_password})
 
-    # Create a Pandas Excel writer using XlsxWriter as the engine.
-    writer = pd.ExcelWriter('../database/database.xlsx', engine='xlsxwriter')
+        # Create a Pandas Excel writer using XlsxWriter as the engine.
+        writer = pd.ExcelWriter('../database/database.xlsx', engine='xlsxwriter')
 
-    # Convert the dataframe to an XlsxWriter Excel object.
-    df.to_excel(writer, sheet_name='Sheet1')
+        # Convert the dataframe to an XlsxWriter Excel object.
+        df.to_excel(writer, sheet_name='Sheet1')
 
-    # Close the Pandas Excel writer and output the Excel file.
-    writer.save()
+        # Close the Pandas Excel writer and output the Excel file.
+        writer.save()
 
-    return True
-
-
-def refreshDatabase():
-  # Create a Pandas dataframe from the excel file
-  df = pd.read_excel('../database/database.xlsx', sheet_name='Sheet1')
-
-  # Save columns as list
-  list_fname = convert(df['First Name'])
-  list_lname = convert(df['Last Name'])
-  list_email = convert(df['Email'])
-  list_password = convert(df['Password'])
-
-  # Create a dictionary (KEY email: VALUE password) for user information
-  dict_email_password = {}
-  for i in range(len(list_email)):
-    dict_email_password[list_email[i]] = list_password[i]
+        return True
 
 def getDatabase():
-  list = []
-  list.append(list_fname)
-  list.append(list_lname)
-  list.append(list_email)
-  list.append(list_password)
-  return(list)
+    list = []
+    list.append(list_fname)
+    list.append(list_lname)
+    list.append(list_email)
+    list.append(list_password)
+    return(list)
+
+start = timeit.default_timer()
 
 # Create a Pandas dataframe from the excel file
 df = pd.read_excel('../database/database.xlsx', sheet_name='Sheet1')
@@ -90,23 +78,28 @@ list_lname = convert(df['Last Name'])
 list_email = convert(df['Email'])
 list_password = convert(df['Password'])
 
+stop = timeit.default_timer()
+print()
+print('Time: ', stop - start)
+print()
+
 # Create a dictionary (KEY email: VALUE password) for user information
 dict_email_password = {}
 for i in range(len(list_email)):
-  dict_email_password[list_email[i]] = list_password[i]
-
+    dict_email_password[list_email[i]] = list_password[i]
+# print("EMAL PASSWORD")
 
 if __name__ == '__main__':
-  print()
+    print()
 
-  print("BEFORE:", list_email)
+    print("BEFORE:", list_email)
 
-  fname = 'Jasmine'
-  lname = 'Mai'
-  email = 'jasmine@gmail.com'
-  password = 'Cat2'
+    fname = 'Jasmine'
+    lname = 'Mai'
+    email = 'jasmine@gmail.com'
+    password = 'Cat2'
 
-  print("USER CREATED:", userCreation(fname, lname, email, password))
-  print("AFTER:", list_email)
+    print("USER CREATED:", userCreation(fname, lname, email, password))
+    print("AFTER:", list_email)
 
-  print("DONE")
+    print("DONE")
